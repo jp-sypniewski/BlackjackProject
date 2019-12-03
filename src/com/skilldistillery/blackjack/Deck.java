952a7f521f02e4cfeba9c052543ed6c0b6961665
jp-sypniewski <jpsypniewski@icloud.com>
2019-12-03 13:17:06 -0700

    updating from UML

--- src/com/skilldistillery/blackjack/Deck.java
+++ src/com/skilldistillery/blackjack/Deck.java
@@ -1,5 +1,38 @@
 package com.skilldistillery.blackjack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+	private List<Card> cards;
+	
+	{
+		cards = new ArrayList<Card>();
+	}
+	
+	public Deck() {
+		
+	}
+	
+	public int cardsLeftInDeck() {
+		return cards.size();
+	}
+	
+	public Card dealCard() {
+		return cards.remove(0);
+	}
+	
+//	public void dealCard(Hand hand) {
+//		
+//	}
+	
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+	
+	public int size() {
+		return 52;
+	}
 
 }
---
+++ src/com/skilldistillery/blackjack/Hand.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.blackjack;
+
+import java.util.List;
+
+public abstract class Hand {
+	List<Card> hand;
+	
+	
+	
+	
+
+}
