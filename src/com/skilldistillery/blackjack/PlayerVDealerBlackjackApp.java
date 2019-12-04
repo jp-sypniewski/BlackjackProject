4416472e90023801c1911755cd77dd3b1e0a5dff
jp-sypniewski <jpsypniewski@icloud.com>
2019-12-04 12:32:38 -0700

    add player v dealer class

---
+++ src/com/skilldistillery/blackjack/PlayerVDealerBlackjackApp.java
@@ -0,0 +1,25 @@
+package com.skilldistillery.blackjack;
+
+import java.util.Scanner;
+
+public class PlayerVDealerBlackjackApp {
+	BlackjackHand playerHand;
+	BlackjackHand dealerHand;
+	Deck deck;
+	Scanner kb;
+	int input;
+	boolean quit = true;
+	
+	
+	
+	
+	public static void main(String[] args) {
+		PlayerVDealerBlackjackApp pvd = new PlayerVDealerBlackjackApp();
+		pvd.run();
+	}
+	
+	public void run() {
+		
+	}
+
+}
